f599320d400d354ea43006744a13e3680100c2c1
zjb19911005 <397563682@qq.com>
2016-03-10 18:56:27 +0800

    demo

--- Firstpage.py
+++ Firstpage.py
@@ -1,7 +1,10 @@
 #coding=utf-8
 __author__ = 'Junior'
-import requests as req
+import urllib2
 import re
 
-html=req.get('http://www.baidu.com/')
-print html.text
+# html=req.get('http://www.baidu.com/')
+# print html.text
+#第一个简单的程序
+print 'my name is junior'
+raw_input('')
