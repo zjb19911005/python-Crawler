e35877c9ea33ffd02132f01ad11846a2d00a8a28
zjb19911005 <397563682@qq.com>
2016-03-11 15:56:17 +0800

    mac提交

--- Firstpage.py
+++ Firstpage.py
@@ -1,8 +1,5 @@
 # coding=utf-8
 __author__ = 'Junior'
-import urllib2 as url2
-import urllib as url1
-import re
 
 #常规传参
 # request=url.Request('http://www.baidu.com/s?cid=qb7.zhuye&ie=utf-8&wd=%E7%8E%8B%E8%80%85%E8%8D%A3%E8%80%80')
