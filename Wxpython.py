75b49d5c67e2110d2c99ddd051ab07bc77ca20f0
zjb19911005 <397563682@qq.com>
2016-05-12 10:09:07 +0800

    初始化wx模块

--- Wxpython.py
+++ Wxpython.py
@@ -21,11 +21,22 @@ import wx#导入wx模块
 # # app.OnInit()手动调用Oninit
 # app.MainLoop()#开始时间循环
 
-class ZhuFame(wx.Frame):
+class On_Tool(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self,None,-1,'ZHU',size=(300,200))
-        label=wx.StaticText(self,-1,'Helloworld')
+        panel=wx.Panel(self,-1)
+        label=wx.StaticText(panel,-1,'Helloworld',(100,10))#一个坐标表示位置
+        label.SetBackgroundColour('black')
+        label.SetForegroundColour('white')
+
+        text1=wx.StaticText(panel,-1,'On Tool',(100,50),(160,-1),wx.ALIGN_CENTER)#第二个数字160表示是text的长度
+        text1.SetBackgroundColour('green')
+        text1.SetForegroundColour('yellow')
+        text2=wx.TextCtrl(panel,-1,u"输入你的名字：",(100,50),(130,-1))
+        text2.SetInsertionPoint(0)
+        png=wx.Image('C:\Users\Administrator.XYZ-PC\Desktop\On Tips\icon.png',wx.BITMAP_TYPE_PNG).ConvertToBitmap()#按钮图片话处理
+        self.button=wx.BitmapButton(panel,-1,png,pos = (0,0))
 app =wx.App()
-zframe=ZhuFame()
-zframe.Show()
+frame=On_Tool()
+frame.Show()
 app.MainLoop()
